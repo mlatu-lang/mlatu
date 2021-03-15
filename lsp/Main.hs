ed47073e675999d546050e7cd7978666f2c84360
Caden Haustein <code@brightlysalty.33mail.com>
2021-03-15 09:31:16 -0500

    Add sample lsp

---
+++ lsp/Main.hs
@@ -0,0 +1,42 @@
+module Main where 
+
+import Relude
+import Language.LSP.Server 
+import Language.LSP.Types 
+
+handlers :: Handlers (LspM ())
+handlers = mconcat
+  [ notificationHandler SInitialized $ \_not -> do
+      let params = ShowMessageRequestParams MtInfo "Turn on code lenses?"
+            (Just [MessageActionItem "Turn on", MessageActionItem "Don't"])
+      _ <- sendRequest SWindowShowMessageRequest params $ \res ->
+        case res of
+          Right (Just (MessageActionItem "Turn on")) -> do
+            let regOpts = CodeLensRegistrationOptions Nothing Nothing (Just False)
+              
+            _ <- registerCapability STextDocumentCodeLens regOpts $ \_req responder -> do
+              let cmd = Command "Say hello" "lsp-hello-command" Nothing
+                  rsp = List [CodeLens (mkRange 0 0 0 100) (Just cmd) Nothing]
+              responder (Right rsp)
+            pure ()
+          Right _ ->
+            sendNotification SWindowShowMessage (ShowMessageParams MtInfo "Not turning on code lenses")
+          Left err ->
+            sendNotification SWindowShowMessage (ShowMessageParams MtError $ "Something went wrong!\n" <> show err)
+      pure ()
+  , requestHandler STextDocumentHover $ \req responder -> do
+      let RequestMessage _ _ _ (HoverParams _doc pos _workDone) = req
+          Position _l _c' = pos
+          rsp = Hover ms (Just range)
+          ms = HoverContents $ markedUpContent "lsp-demo-simple-server" "Hello world"
+          range = Range pos pos
+      responder (Right $ Just rsp)
+  ]
+
+main :: IO Int
+main = runServer $ ServerDefinition 
+    { onConfigurationChange = const $ pure $ Right (),
+    doInitialize = \env _req -> pure $ Right env,
+    staticHandlers = handlers, 
+    interpretHandler = \env -> Iso (runLspT env) liftIO,
+    options = defaultOptions}
