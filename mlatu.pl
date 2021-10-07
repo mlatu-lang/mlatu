b6fa4232481e0be3d4fbcc20e985c3043dc476f2
Caden Haustein <code@brightlysalty.33mail.com>
2021-10-07 17:51:43 -0500

    Increase efficiency of generated Prolog

--- mlatu.pl
+++ mlatu.pl
@@ -1,39 +1,42 @@
 :- module(mlatu, [rewrite/2, user_equiv/2]).
 :- dynamic([user_equiv/2], [multifile, discontiguous]).
 
-equiv(Xs, Other) :-
-    user_equiv(Xs, Other) ;
-    builtin_equiv(Xs, Other).
+equiv(Xs, Other) :- user_equiv(Xs, Other).
 
-builtin_equiv(['d',X|Xs], Other) :-
+equiv(['d',X|Xs], Other) :-
     equiv([X,X|Xs], Other).
 
-builtin_equiv(['r',_|Xs], Other) :-
+equiv(['r',_|Xs], Other) :-
     equiv(Xs, Other).
 
-builtin_equiv(['u',Quote|Xs], Other) :-
-    is_list(Quote),
-    append(Quote, Xs, NewXs),
-    equiv(NewXs, Other).
+equiv(['u',[]|Xs], Other) :- 
+    equiv(Xs, Other).
+equiv(['u',[First|Rest]|Xs], Other) :-
+    append([First|Rest], Xs, NewXs), equiv(NewXs, Other)
+    .
 
-builtin_equiv(['q',X|Xs], Other) :-
+equiv(['q',X|Xs], Other) :-
     equiv([[X]|Xs], Other).
 
-builtin_equiv(['c',Quote1,Quote2|Xs], Other) :-
-    is_list(Quote1), is_list(Quote2),
-    append(Quote1, Quote2, NewQuote),
+equiv(['c',[],[]|Xs], Other) :-
+    equiv([[]|Xs], Other).
+equiv(['c',[],[First|Rest]|Xs], Other) :-
+    equiv([[First|Rest]|Xs], Other).
+equiv(['c',[First|Rest],[]|Xs], Other) :-
+    equiv([[First|Rest]|Xs], Other).
+equiv(['c',[AFirst|ARest],[BFirst|BRest]|Xs], Other) :-
+    append([AFirst|ARest], [BFirst|BRest], NewQuote),
     equiv([NewQuote|Xs], Other).
 
-builtin_equiv(['s',A,B|Xs], Other) :-
+equiv(['s',A,B|Xs], Other) :-
     equiv([B,A|Xs],Other).
 
-builtin_equiv([], []).
-builtin_equiv([X|Xs], Other) :-
+equiv([], []).
+equiv([X|Xs], Other) :-
     equiv(Xs, New),
-    (Xs \= New ->
-        equiv([X|New], Other) ;
-        Other = [X|Xs]).
+    (Xs = New ->
+         Other = [X|Xs] ;
+    equiv([X|New], Other)).
 
 rewrite(List, Other) :-
-    equiv(List, Other),
-    List \= Other.
+    equiv(List, Other), List \= Other, !.
