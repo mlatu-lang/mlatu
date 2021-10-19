e5cfbb7391c0335e19c5eea6118cab3d3555abb0
Caden Haustein <code@brightlysalty.33mail.com>
2021-10-19 18:58:55 -0500

    Update erlang.rs to work with merged changes

--- src/erlang.rs
+++ src/erlang.rs
@@ -10,7 +10,8 @@ use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use crate::ast::{Rule, Term};
 
 pub const BEFORE:&str = "-module(mlatu). -export([interact/1]). 
-spawn_rewrite(List, IfOk, IfNone) -> Self = self(), spawn(fun() -> inner_rewrite(List, Self) end), receive {ok, NewRest} -> IfOk(NewRest); {none} -> IfNone() end.
+spawn_rewrite(List, IfOk, IfNone) -> Self = self(), spawn(fun() -> inner_rewrite(List, Self) end), \
+                         receive {ok, NewRest} -> IfOk(NewRest); {none} -> IfNone() end.
 no_rewrite(Pid) -> Pid ! {none}.
 rewrite_to(Pid, New) -> Pid ! {ok, New}.
 inner_rewrite(List, Parent) -> case List of ";
@@ -19,11 +20,13 @@ pub const AFTER:&str = "
   [] -> no_rewrite(Parent);
   [{word, u}, {quote, List} | Rest] -> rewrite_to(Parent, List ++ Rest);
   [{word, q}, A | Rest] -> rewrite_to(Parent, [{quote, [A]} | Rest]);
-  [{word, c}, {quote, List1}, {quote, List2} | Rest] -> rewrite_to(Parent, [{quote, List1 ++  List2} | Rest]);
+  [{word, c}, {quote, List1}, {quote, List2} | Rest] -> rewrite_to(Parent, [{quote, List1 ++  \
+                        List2} | Rest]);
   [{word, d}, A | Rest] -> rewrite_to(Parent, [A, A | Rest]);
   [{word, r}, _ | Rest] -> rewrite_to(Parent, Rest);
   [{word, s}, A, B | Rest] -> rewrite_to(Parent, [B, A | Rest]);
-  [First | Rest] -> spawn_rewrite(Rest, fun(NewRest) -> spawn(fun() -> inner_rewrite([First | NewRest], Parent) end) end, fun() -> no_rewrite(Parent) end)
+  [First | Rest] -> spawn_rewrite(Rest, fun(NewRest) -> spawn(fun() -> inner_rewrite([First | \
+                        NewRest], Parent) end) end, fun() -> no_rewrite(Parent) end)
     end.
 print_term({word, Atom}) -> atom_to_list(Atom);
 print_term({quote, List}) -> \"(\" ++ print_terms(List) ++ \")\".
@@ -67,9 +70,9 @@ pub async fn run(rules:Vec<Rule>, sender:UnboundedSender<Vec<Term>>,
                  mut receiver:UnboundedReceiver<Vec<Term>>) {
   let mut string = BEFORE.to_string();
   for rule in rules {
-    string.push_str(&translate_terms(rule.0, "|Rest"));
+    string.push_str(&translate_terms(rule.pat, "|Rest"));
     string.push_str(" -> rewrite_to(Parent, ");
-    string.push_str(&translate_terms(rule.1, "|Rest"));
+    string.push_str(&translate_terms(rule.rep, "|Rest"));
     string.push_str(");");
   }
   string.push_str(AFTER);
@@ -78,10 +81,11 @@ pub async fn run(rules:Vec<Rule>, sender:UnboundedSender<Vec<Term>>,
     let mut stdin = child.stdin.take().expect("child did not have a handle to stdin");
     let stdout = child.stdout.take().expect("child did not have a handle to stdout");
     let mut reader = BufReader::new(stdout).lines();
-    assert_eq!(reader.next_line().await.unwrap(),
+    assert_eq!(reader.next_line().await.expect("erlang process aborted unexpectedly"),
                Some("Eshell V12.1.2  (abort with ^G)".to_string()));
     stdin.write_all(b"c(\"mlatu\").\n").await.expect("could not write to stdin");
-    assert_eq!(reader.next_line().await.unwrap(), Some("1> {ok,mlatu}".to_string()));
+    assert_eq!(reader.next_line().await.expect("erlang process aborted unexpectedly"),
+               Some("1> {ok,mlatu}".to_string()));
     while let Some(before) = receiver.recv().await {
       let term = translate_terms(before, "");
       stdin.write_all(format!("mlatu:interact({}).\n", term).as_bytes())
@@ -92,7 +96,7 @@ pub async fn run(rules:Vec<Rule>, sender:UnboundedSender<Vec<Term>>,
         sender.send(result.0).expect("send error");
       }
     }
-    child.wait().await.unwrap();
+    child.wait().await.expect("child could not be waited upon");
   } else {
     panic!("Could not spawn 'erl' process")
   }
