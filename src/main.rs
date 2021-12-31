2a8169229c838f6703a2704e1df0cb188b047ba9
Caden Haustein <code@brightlysalty.33mail.com>
2021-12-31 13:52:49 -0600

    Rewrite (pun intended) to use the library

--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,259 @@
-#![feature(with_options)]
+#![feature(derive_default_enum)]
+#![deny(clippy::correctness)]
+#![warn(clippy::suspicious,
+        clippy::style,
+        clippy::complexity,
+        clippy::perf,
+        clippy::pedantic,
+        clippy::nursery)]
 
-use std::fs::File;
-use std::io::Read;
-use std::path::{Path, PathBuf};
+use iced::keyboard::KeyCode;
+use iced::{button, executor, keyboard, text_input, tooltip, Application, Button, Clipboard, Column, Command, Element, Length, Radio, Row, Settings, Space, Subscription, Text, TextInput, Tooltip};
+use iced_native::{subscription, Event};
+use im::{vector, Vector};
+use mlatu::{parse, pretty, rewrite, Arena, Rodeo, Term};
 
-use clap::{App, Arg, SubCommand};
-use mlatu::{binary, erlang, parse_rules, Editor, Interactive, Rule};
-use tokio::sync::mpsc::unbounded_channel;
+macro_rules! button {
+  ($condition:expr, $state:expr, $symbol:expr, $name:expr, $message:ident) => {
+    Tooltip::new(if ($condition).is_some() {
+                   Element::from(Button::new(&mut $state, Text::new($symbol).size(50)).on_press(Message::$message)
+                                                                        .width(Length::Units(35)))
+                 } else {
+                   Element::from(Text::new($symbol).size(50).width(Length::Units(35)))
+                 },
+                 $name,
+                 tooltip::Position::Bottom).into()
+  };
+}
 
-fn load_text_file(filename:&str) -> Result<Vec<Rule>, String> {
-  match std::fs::read(filename) {
-    | Ok(bytes) => match String::from_utf8(bytes) {
-      | Ok(string) => match parse_rules(&string) {
-        | Ok(rules) => Ok(rules),
-        | Err(e) => Err(format!("Error while parsing '{}': {}", filename, e)),
+#[derive(Debug, Clone)]
+enum Message {
+  TextInputChanged(String),
+  TextInputSubmitted,
+  RadioSelected(usize),
+  PlusPressed,
+  MinusPressed,
+  WrapPressed,
+  UnwrapPressed,
+  SwapPressed,
+  CombinePressed,
+  UpPressed,
+  DownPressed,
+}
+
+#[derive(Default)]
+struct App {
+  input:Vector<Term>,
+  output:Vector<Term>,
+  rodeo:Rodeo,
+  arena:Arena,
+
+  text_value:String,
+  text_input:text_input::State,
+  plus_button:button::State,
+  minus_button:button::State,
+  wrap_button:button::State,
+  unwrap_button:button::State,
+  swap_button:button::State,
+  combine_button:button::State,
+  selected:Option<usize>,
+}
+
+impl App {
+  fn update_output(&mut self) {
+    self.output = rewrite(&self.arena, &self.rodeo, &Vector::new(), self.input.clone());
+  }
+}
+
+fn unwrap_valid(selected:Option<usize>, input:&Vector<Term>) -> Option<(usize, Vector<Term>)> {
+  if let Some(index) = selected {
+    if let Term::Quote(terms) = &input[index] {
+      return Some((index, terms.clone()))
+    }
+  }
+  None
+}
+
+fn swap_valid(selected:Option<usize>, input:&Vector<Term>) -> Option<(usize, usize)> {
+  if let Some(index) = selected {
+    if index + 1 < input.len() {
+      return Some((index, index + 1))
+    }
+  }
+  None
+}
+
+fn combine_valid(selected:Option<usize>, input:&Vector<Term>)
+                 -> Option<(usize, Vector<Term>, Vector<Term>)> {
+  if let Some(index) = selected {
+    if let Term::Quote(a) = &input[index] {
+      if let Some(Term::Quote(b)) = input.get(index + 1) {
+        return Some((index, a.clone(), b.clone()))
+      }
+    }
+  }
+  None
+}
+
+impl Application for App {
+  type Executor = executor::Default;
+  type Flags = ();
+  type Message = Message;
+
+  fn new(_:()) -> (Self, Command<Message>) {
+    (Self::default(), Command::none())
+  }
+
+  fn subscription(&self) -> Subscription<Message> {
+    subscription::events_with(|event, _status| match event {
+      | Event::Keyboard(keyboard::Event::KeyPressed { key_code: KeyCode::Up, modifiers: _, }) =>
+        Some(Message::UpPressed),
+      | Event::Keyboard(keyboard::Event::KeyPressed { key_code: KeyCode::Down, modifiers: _, }) =>
+        Some(Message::DownPressed),
+      // | Event::Keyboard(keyboard::Event::CharacterReceived('+')) => Some(Message::PlusPressed),
+      // | Event::Keyboard(keyboard::Event::CharacterReceived('-')) => Some(Message::MinusPressed),
+      // | Event::Keyboard(keyboard::Event::CharacterReceived('>')) => Some(Message::WrapPressed),
+      // | Event::Keyboard(keyboard::Event::CharacterReceived('<')) => Some(Message::UnwrapPressed),
+      // | Event::Keyboard(keyboard::Event::CharacterReceived('~')) => Some(Message::SwapPressed),
+      // | Event::Keyboard(keyboard::Event::CharacterReceived(',')) =>
+      // Some(Message::CombinePressed),
+      | _ => None,
+    })
+  }
+
+  fn title(&self) -> String { "mlatu app".to_string() }
+
+  fn view(&mut self) -> Element<Message> {
+    Column::with_children(vec![
+            TextInput::new(&mut self.text_input, "Insert word...", &self.text_value, Message::TextInputChanged).on_submit(Message::TextInputSubmitted).into(),
+                Row::with_children(vec![
+                    button!(self.selected, self.plus_button, "+", "copy", PlusPressed),
+                    button!(self.selected, self.minus_button, "-", "remove", MinusPressed),
+                    button!(self.selected, self.wrap_button, ">", "wrap", WrapPressed),
+                    button!(unwrap_valid(self.selected, &self.input), self.unwrap_button, "<", "unwrap", UnwrapPressed),
+                    button!(swap_valid(self.selected, &self.input), self.swap_button, "~", "swap", SwapPressed),
+                    button!(combine_valid(self.selected, &self.input), self.combine_button, ",", "combine", CombinePressed)
+                ])
+            .into(),
+            Space::with_height(Length::Units(30)).into(),
+            Row::new()
+                .spacing(30)
+                .push(
+                    Column::new()
+                        .push(Text::new("Input"))
+                        .push(Column::with_children(
+                            self.input
+                                .iter_mut()
+                                .enumerate()
+                                .map(|(i, term)| {
+                                    Radio::new(
+                                        i,
+                                        pretty::term(&self.rodeo, term.clone()),
+                                        self.selected,
+                                        Message::RadioSelected,
+                                    )
+                                    .text_size(40)
+                                    .into()
+                                })
+                                .collect(),
+                        )),
+                )
+                .push(
+                    Column::new()
+                        .push(Text::new("Output"))
+                        .push(Column::with_children(
+                            self.output
+                                .iter()
+                                .map(|term| {
+                                    Text::new(pretty::term(&self.rodeo, term.clone()))
+                                        .size(40)
+                                        .into()
+                                })
+                                .collect(),
+                        )),
+                )
+                .into(),
+        ])
+        .into()
+  }
+
+  fn update(&mut self, message:Message, _clipboard:&mut Clipboard) -> Command<Message> {
+    match message {
+      | Message::TextInputChanged(s) => {
+        self.text_value = s;
       },
-      | Err(e) => Err(format!("Error in decoding '{}': {}", filename, e)),
-    },
-    | Err(e) => Err(format!("Error while reading '{}': {}", filename, e)),
-  }
-}
-
-fn load_binary_file(filename:&str) -> Result<Vec<Rule>, String> {
-  match File::with_options().append(true).create(true).read(true).open(filename) {
-    | Ok(mut file) => {
-      let mut buf = Vec::new();
-      match file.read_to_end(&mut buf) {
-        | Ok(_) =>
-          if buf.is_empty() {
-            Ok(vec![])
-          } else {
-            match binary::deserialize_rules(&mut buf) {
-              | Some(rules) => Ok(rules),
-              | None => Err(format!("Error while deserializing {}", filename)),
-            }
-          },
-        | Err(e) => Err(format!("Error while reading '{}': {}", filename, e)),
-      }
-    },
-    | Err(e) => Err(format!("Error while opening '{}': {}", filename, e)),
-  }
-}
-
-fn load_file(filename:&str) -> Result<Vec<Rule>, String> {
-  let path = Path::new(&filename);
-  match path.extension().unwrap().to_str().unwrap() {
-    | "mlt" => load_text_file(filename),
-    | "mlb" => load_binary_file(filename),
-    | ext => Err(format!("Unrecognized file extension: {}", ext)),
-  }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), String> {
-  let matches = App::new("mlatu")
-                 .version("0.1")
-                 .author("Caden Haustein <code@brightlysalty.33mail.com>")
-                 .about("the mlatu language interface")
-                 .arg(Arg::with_name("FILES").multiple(true).help("Sets the rule files to use")).
-                  arg(Arg::with_name("no-prelude").long("no-prelude").help("Doesn't load the normal prelude"))
-                  .subcommand(SubCommand::with_name("edit")
-                    .about("the mlatu structured editor")
-                    .version("0.1")
-                    .author("Caden Haustein <code@brightlysalty.33mail.com>")
-                    .arg(Arg::with_name("FILE").required(true).help("Sets the rule file to edit")
-                  ))
-                .get_matches();
-
-  match matches.subcommand() {
-    | ("edit", Some(sub_matches)) => {
-      let filename = sub_matches.value_of("FILE").unwrap();
-      let rules = load_file(filename)?;
-      let mut path = PathBuf::from(filename);
-      let _ = path.set_extension("mlb");
-      match path.canonicalize() {
-        | Ok(path) => Editor::new(path, &rules)?.run().await,
-        | Err(_) => eprintln!("Path could not be canonicalized"),
-      }
-    },
-    | _ => {
-      let mut files = Vec::new();
-      if !matches.is_present("no-prelude") {
-        files.push("prelude.mlb".to_string());
-      }
-      if let Some(args) = matches.values_of("FILES") {
-        files.extend(args.map(ToOwned::to_owned));
-      }
-      match files.into_iter().map(|file| load_file(&file)).collect::<Result<Vec<_>, _>>() {
-        | Ok(rules) => {
-          let rules:Vec<_> = rules.into_iter().flatten().collect();
-          let (interactive_sender, erlang_receiver) = unbounded_channel();
-          let (erlang_sender, interactive_receiver) = unbounded_channel();
-          tokio::spawn(async move { erlang::run(rules, erlang_sender, erlang_receiver).await });
-          match Interactive::new() {
-            | Ok(mut interactive) =>
-              interactive.run(interactive_sender, interactive_receiver).await,
-            | Err(error) => eprintln!("{}", error),
+      | Message::TextInputSubmitted => {
+        if let Ok(term) = parse::term(&self.arena, &self.rodeo, &self.text_value) {
+          self.input.insert(self.selected.unwrap_or(0), term.clone());
+          self.update_output();
+          self.text_value = String::new();
+        }
+      },
+      | Message::RadioSelected(index) => {
+        self.selected = Some(index);
+      },
+      | Message::PlusPressed =>
+        if let Some(index) = self.selected {
+          self.input.insert(index, self.input[index].clone());
+          self.update_output();
+        },
+      | Message::MinusPressed =>
+        if let Some(index) = self.selected {
+          self.input.remove(index);
+          self.update_output();
+          if index >= self.input.len() {
+            self.selected = None;
           }
         },
-        | Err(error) => eprintln!("{}", error),
-      }
-    },
+      | Message::WrapPressed =>
+        if let Some(index) = self.selected {
+          let term = Term::make_quote(&self.arena, vector![self.input[index].clone()]);
+          self.input[index] = term.clone();
+          self.update_output();
+        },
+      | Message::UnwrapPressed =>
+        if let Some((index, terms)) = unwrap_valid(self.selected, &self.input) {
+          self.input.remove(index);
+          for term in terms {
+            self.input.insert(index, term.clone());
+          }
+          self.update_output();
+          if index >= self.input.len() {
+            self.selected = None;
+          }
+        },
+      | Message::SwapPressed =>
+        if let Some((index1, index2)) = swap_valid(self.selected, &self.input) {
+          self.input.swap(index1, index2);
+          self.update_output();
+        },
+      | Message::CombinePressed =>
+        if let Some((index, mut terms, second_terms)) = combine_valid(self.selected, &self.input) {
+          self.input.remove(index + 1);
+          terms.extend(second_terms);
+          self.input[index] = Term::make_quote(&self.arena, terms.clone()).clone();
+          self.update_output();
+          if index >= self.input.len() {
+            self.selected = None;
+          }
+        },
+      | Message::UpPressed => {
+        self.selected = Some(if let Some(index) = self.selected {
+                               index.saturating_sub(1)
+                             } else {
+                               self.input.len().saturating_sub(1)
+                             });
+      },
+      | Message::DownPressed => {
+        self.selected = Some(if let Some(index) = self.selected {
+                               self.input.len().saturating_sub(1).min(index + 1)
+                             } else {
+                               0
+                             });
+      },
+    };
+    Command::none()
   }
-
-  Ok(())
 }
+
+fn main() -> iced::Result { App::run(Settings::default()) }
